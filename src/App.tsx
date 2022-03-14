d03b42516685826184f5bfc688c155d686e07978
Jordan Pectyo <mrpectyo@gmail.com>
2022-03-13 22:43:45 -0400

    Updated packages, also modified rules for templ

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React, {FC} from 'react';
+
+import React, { FC } from 'react'
 
 export const App: FC = () => {
   return (
-    <h1>If you're seeing this you have the right page!</h1>
-  );
-};
+    <h1>If you&apos;re seeing this you have the right page!</h1>
+  )
+}
 
-export default App;
+export default App
